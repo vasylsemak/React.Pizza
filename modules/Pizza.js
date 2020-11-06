16a7a0301232f5552b66cbe24630556da3fe8a82
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-05 21:49:32 -0500

    create button and handleClick function to change favourite topping

--- modules/Pizza.js
+++ modules/Pizza.js
@@ -5,17 +5,25 @@ class Pizza extends Component {
   constructor() {
     super()
     this.state = { favTopping: "Mashrooms" }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    let clickedText = event.target.innerText;
+    this.setState(()=> ({ favTopping: clickedText }));
   }
 
   render() {
     return (
       <Fragment>
         <h1>Your favourite pizza topping is: {this.state.favTopping}</h1>
-        <ul>
-          <li><Topping favor={this.state.favTopping} topping="Onions" /></li>
-          <li><Topping favor={this.state.favTopping} topping="Mashrooms" /></li>
-          <li><Topping favor={this.state.favTopping} topping="Pepperoni" /></li>
-        </ul>
+        <div onClick={this.handleClick}>
+          <ul>
+            <li><Topping favor={this.state.favTopping} topping="Onions" /></li>
+            <li><Topping favor={this.state.favTopping} topping="Mashrooms" /></li>
+            <li><Topping favor={this.state.favTopping} topping="Pepperoni" /></li>
+          </ul>
+        </div>
       </Fragment>
     )
   }
